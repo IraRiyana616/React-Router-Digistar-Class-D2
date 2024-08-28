8eebc07be065d47b51a1afe879fa8b180d5e8916
Ira Riyana <irariyanasari@gmail.com>
2024-08-28 20:59:40 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import Media from './components/Media';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          {/* Tambahkan Link ke masing-masing halaman yang ada dibawah ini:*/}
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contacts">Contacts</Link>
+              </li>
+              <li>
+                <Link to="/media">Media</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<h1>Welcome</h1>}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contacts" element={<Contacts />}></Route>
+          <Route path="/media" element={<Media />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
